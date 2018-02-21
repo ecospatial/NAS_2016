14fff104305ad19f9543659f1a99a2322b55dc98
Tyler Hardy <tyler.t.hardy@eagles.usm.edu>
2018-02-21 16:09:25 -0600

    DataCreation/WetlandChange: Allow for customization in change attributes (land type choices for 96 and 06)

--- DataCreation/WetlandChange.R
+++ DataCreation/WetlandChange.R
@@ -3,14 +3,18 @@ library(sp)
 library(raster)
 
 # Configuration -----------------------------------------------------------
-outputName = "WetToAny"
+outputName = "PaluEstuToWaterAquatic"
+
+fromRegex = "^.*(Palustrine|Estuarine) (?!Aquatic)"
+toRegex = "^.*(Water|Aquatic)"
+fromLaymen = "Land types in 1996 that are palustine or estuarine, except for palu/estu aquatic beds."
+toLaymen = "Land types in 2006 that are open water or palu/estu aquatic beds."
+
 test = F
 
 
 # Initialization ----------------------------------------------------------
 dir = sprintf("C:/DATA/CCAP/T1/%s", outputName)
-paluEstuDir = paste0(dir, "/PaluEstu96")
-estuDir = paste0(dir, "/Estu96")
 
 
 # Load CCAP Data ----------------------------------------------------------
@@ -19,68 +23,56 @@ extension = "_1996_2006_ccap_change.img"
 ccap = list(
   la = raster(paste0(ccapDir,"/la",extension)),
   fl = raster(paste0(ccapDir,"/fl",extension)),
-  ms = raster(paste0(ccapDir,"/ms",extension)),
+  ms = raster(paste0(ccapDir,"/ms",extesion)),
   tx = raster(paste0(ccapDir,"/tx",extension)),
   al = raster(paste0(ccapDir,"/al",extension))
 )
 wetlandCodes = read.delim(paste0(ccapDir, "/wetlandCodes.txt")) #all changes to and from wetlands
 
 
+
 # Test --------------------------------------------------------------------
 if (test)
-  {
+{
+  rowColSize = 500
+  cellRange = 500
   ccap = list(
-      la = raster(nrow=5,ncol=5),
-      fl = raster(nrow=5,ncol=5),
-      al = raster(nrow=5,ncol=5),
-      ms = raster(nrow=5,ncol=5),
-      tx = raster(nrow=5,ncol=5)
+      la = raster(nrow=rowColSize,ncol=rowColSize),
+      fl = raster(nrow=rowColSize,ncol=rowColSize),
+      al = raster(nrow=rowColSize,ncol=rowColSize),
+      ms = raster(nrow=rowColSize,ncol=rowColSize),
+      tx = raster(nrow=rowColSize,ncol=rowColSize)
     )
   ccap = lapply(ccap, function(x){
-    r=raster(nrow=5,ncol=5)
-    r[] = ceiling(runif(ncell(r))*500)
+    r=raster(nrow=rowColSize,ncol=rowColSize)
+    r[] = ceiling(runif(ncell(r))*cellRange)
     x=r
   })
 }
 
 
-# Extract Areas with Palustrine or Estuarine Wetlands in 1996 -------------
-paluEstuCodes96 = wetlandCodes[grep("Wetland", wetlandCodes$Type96),] #land types that were wetland in 96
-paluEstu96 = lapply(ccap, function(x){
-  return(x * x %in% paluEstuCodes96$ID)
-})
+# Filter land change codes to types specified in config above -------------
+fromCodes = wetlandCodes[grep(fromRegex, wetlandCodes$Type96, perl=T),]
+toCodes = wetlandCodes[grep(toRegex, wetlandCodes$Type06, perl=T),]
+changeCodes = fromCodes[fromCodes$ID %in% toCodes$ID,]
 
-# Extract Areas with only Estuarine Wetlands in 1996 ----------------------
-estuCodes96 = wetlandCodes[grep("Estuarine", wetlandCodes$Type96),]
-estu96 = lapply(ccap, function(x){
-  return(x * x %in% estuCodes96$ID)
+change9606 = lapply(ccap, function(x){
+  return(x * x %in% changeCodes$ID)
 })
 
 
 # Save Rasters ------------------------------------------------------------
-if(!dir.exists(dir))
+if(dir.exists(dir) && length(list.files(dir)) != 0)
 {
+  warning(paste0(outputName, " directory already exists and is not empty; files not written."))
+} else {
   dir.create(dir)
+  for (i in 1:length(change9606))
+  {
+    stateName = names(change9606)[i]
+    writeRaster(change9606[[i]], sprintf("%s/%s_%s.img", dir, stateName, outputName), datatype="INT2U", options="COMPRESS=RLE")
+  }
 }
-
-if(!dir.exists(paluEstuDir) || length(list.files(paluEstuDir)) == 0)
-{
-  dir.create(paluEstuDir)
-  for(i in 1:length(paluEstu96)){
-    stateName = names(paluEstu96)[i]
-    writeRaster(paluEstu96[[i]], sprintf("%s/%s_paluEstu96.img", paluEstuDir, stateName), datatype="INT2U", options="COMPRESS=RLE")
-  }
-}else{warning("PaluEstu96 directory already exists and is not empty; files not written.")}
-
-if(!dir.exists(estuDir) || length(list.files(estuDir)) == 0)
-{
-  dir.create(estuDir)
-  for(i in 1:length(estu96)){
-    stateName = names(estu96)[i]
-    writeRaster(estu96[[i]], sprintf("%s/%s_estu96.img", estuDir, stateName), datatype="INT2U", options="COMPRESS=RLE")
-  }
-}else{warning("Estu96 directory already exists and is not empty; files not written.")}
-
 
 
 # THK99 -------------------------------------------------------------------
@@ -99,31 +91,21 @@ inlandbuff = get_postgis_query(con, "SELECT * FROM thkbuffers", geom_name = "geo
 
 dbDisconnect(con)
 
-if(!exists("paluEstu96") || !exists("estu96"))
+if(!exists("change9606"))
 {
-  extension = "_estu96.img"
-  estu96 = list(
-    la = raster(paste0(estuDir,"/la",extension)),
-    fl = raster(paste0(estuDir,"/fl",extension)),
-    ms = raster(paste0(estuDir,"/ms",extension)),
-    tx = raster(paste0(estuDir,"/tx",extension)),
-    al = raster(paste0(estuDir,"/al",extension))
-  )
-  
-  extension = "_paluEstu96.img"
-  paluEstu96 = list(
-    la = raster(paste0(paluEstuDir,"/la",extension)),
-    fl = raster(paste0(paluEstuDir,"/fl",extension)),
-    ms = raster(paste0(paluEstuDir,"/ms",extension)),
-    tx = raster(paste0(paluEstuDir,"/tx",extension)),
-    al = raster(paste0(paluEstuDir,"/al",extension))
+  extension = sprintf("_%s.img", outputName)
+  change9606 = list(
+    la = raster(paste0(dir, "/la", extension)),
+    fl = raster(paste0(dir, "/fl", extension)),
+    ms = raster(paste0(dir, "/ms", extension)),
+    tx = raster(paste0(dir, "/tx", extension)),
+    al = raster(paste0(dir, "/al", extension))
   )
 }
 
-paluEstuLoss9606 = data.frame(FID = numeric(0), wetLoss = numeric(0))
-estuLoss9606 = data.frame(FID = numeric(0), wetLoss = numeric(0))
+changeVals9606 = data.frame(FID = numeric(0), wetLoss = numeric(0))
 
-inlandbuff = spTransform(inlandbuff, proj4string(ccap$la))
+inlandbuff = spTransform(inlandbuff, proj4string(ccap$al))
 
 source("RUtilityFunctions/getMinExtent.R")
 for (state in names(ccap))
@@ -131,26 +113,22 @@ for (state in names(ccap))
   e = getMinExtent(inlandbuff, ccap[[state]])
   
   cropLandPts = crop(inlandbuff, e)
-  cropEstu = crop(estu96[[state]], e)
-  cropPaluEstu = crop(paluEstu96[[state]], e)
+  cropChange = crop(change9606[[state]], e)
   
-  estuVals = extract(cropEstu, cropLandPts, fun=function(x, na.rm = na.rm){return(sum(x > 0, na.rm = na.rm))}, na.rm=TRUE)
-  paluEstuVals = extract(cropPaluEstu, cropLandPts, fun=function(x, na.rm = na.rm){return(sum(x > 0, na.rm = na.rm))}, na.rm=TRUE)
+  changeVals = extract(cropChange, cropLandPts, fun=function(x, na.rm = na.rm){return(sum(x > 0, na.rm = na.rm))}, na.rm=TRUE)
   
-  eVals = data.frame(FID = cropLandPts$ORIG_FID, wetLoss = estuVals)
-  estuLoss9606 = rbind(estuLoss9606, eVals)
-  peVals = data.frame(FID = cropLandPts$ORIG_FID, wetLoss = paluEstuVals)
-  paluEstuLoss9606 = rbind(paluEstuLoss9606, peVals)
+  cVals = data.frame(FID = cropLandPts$ORIG_FID, wetLoss = changeVals)
+  changeVals9606 = rbind(changeVals9606, cVals)
 }
 
-write.table(estuLoss9606, sprintf("%s/%s_estu.txt", dir, outputName), row.names = FALSE, quote = FALSE)
-write.table(paluEstuLoss9606, sprintf("%s/%s_paluEstu.txt", dir, outputName), row.names = FALSE, quote = FALSE)
+write.table(changeVals9606, sprintf("%s/%s_output.txt", dir, outputName), row.names = FALSE, quote = FALSE)
 
-estuLoss9606 = estuLoss9606[order(estuLoss9606$FID),]
-cols = colorRampPalette(c("green", "yellow", "red"))(20)
-colBreaks = cut(estuLoss9606$wetLoss, c(-Inf, seq(0, max(estuLoss9606$wetLoss), length.out = 20)))
-plot(inlandbuff)
-plot(inlandbuff, add=T, col=cols[colBreaks], border=NA)
+changeVals9606 = changeVals9606[order(changeVals9606$FID),]
+breaks = 3
+cols = colorRampPalette(c("green", "yellow", "red"))(breaks)
+colBreaks = cut(changeVals9606$wetLoss, c(-Inf, seq(0, max(changeVals9606$wetLoss), length.out = breaks)))
+plot(inlandbuff[inlandbuff$ORIG_FID %in% changeVals9606$FID,])
+plot(inlandbuff[inlandbuff$ORIG_FID %in% changeVals9606$FID,], add=T, col=cols[colBreaks], border=NA)
 
 # r = ccap$ms
 # inlandbuff = spTransform(inlandbuff, proj4string(ccap$ms))
