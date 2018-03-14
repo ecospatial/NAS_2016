01d09dca08fe3744014802537a6b5f2b8ad2f5ec
Tyler Hardy <tylerthardy@gmail.com>
2018-03-14 09:55:32 -0500

    DataCreation/processEE: Add kml processing for thk99buff.kml

--- DataCreation/processEE.R
+++ DataCreation/processEE.R
@@ -1,12 +1,14 @@
-thk99 = read.csv("T0/thk99.csv")
+dir = "X:/NAS Stuff/DATA/EarthEngine/"
 
-wetloss = read.csv("T0/thk99wetloss.csv")
+thk99 = read.csv(paste0(dir, "T0/thk99.csv"))
+
+wetloss = read.csv(paste0(dir, "T0/thk99wetloss.csv"))
 names(wetloss) = gsub("count","WET",names(wetloss))
 
-ndvi = read.csv("T0/thk99NDVI.csv")
+ndvi = read.csv(paste0(dir, "T0/thk99NDVI.csv"))
 names(ndvi) = gsub("mean","ndvi",names(ndvi))
 
-ndmi = read.csv("T0/thk99NDMI.csv")
+ndmi = read.csv(paste0(dir, "T0/thk99NDMI.csv"))
 names(ndmi) = gsub("mean","ndmi",names(ndmi))
 
 data = merge(merge(merge(thk99,wetloss,by="system.index"),ndvi,by="system.index"),ndmi,by="system.index")
@@ -18,10 +20,20 @@ names(data)
 data$logWET = rep(NA, nrow(data))
 data$logWET[data$WET > 0] = log(data$WET[data$WET > 0])
 
-write.csv(data,"T1/fullData.csv")
+write.csv(data, paste0(dir, "T1/fullData.csv"))
 
 
+# Modify kml file for R
+kmlDat = readLines(file(paste0(dir, "T0/thk99buff.kml")))
+kmlDatNew = kmlDat[1:3]
+kmlDatNew = c(kmlDatNew, "<Folder><name>thk99buff</name>")
+kmlDatNew = c(kmlDatNew, kmlDat[4:(length(kmlDat)-2)])
+kmlDatNew = c(kmlDatNew, "</Folder>")
+kmlDatNew = c(kmlDatNew, kmlDat[(length(kmlDat)-2+1):length(kmlDat)])
+writeLines(kmlDatNew, paste0(dir, "T1/thk99buff.kml"))
 
+
+# Plots
 plot(WET~RSLR, data=data)
 plot(WET~NDVI, data=data, ylim=c(0,500),xlim=c(-1,1))
 plot(WET~NDMI, data=data, ylim=c(0,500),xlim=c(-1,1))
