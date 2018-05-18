0ac648a4fc8cb821154fa9efadbe1332b733c169
Tyler Hardy <tyler.t.hardy@eagles.usm.edu>
2018-05-18 16:59:28 -0500

    BayesianTools: Add significance examining function

--- RUtilityFunctions/bayesianTools.R
+++ RUtilityFunctions/bayesianTools.R
@@ -8,7 +8,142 @@ getCIs = function(modelNo, modelName, prob=0.95, ...){
   #return(output)
 }
 
-DICexamine = function(modelName, omit=NA, all=F, top=NULL){
+getCI = function(modelNo, modelName = NULL){
+  if (is.null(modelName))
+  {
+    load(file=sprintf("Results/%s.RData", modelNo))
+    return(output)
+  }
+  load(file=sprintf("Results/%s/%s.RData", modelName, modelNo))
+  return(output)
+}
+
+rescrapeDIC = function(modelName)
+{
+  resultsDir = sprintf("Results/%s/", modelName)
+  write.table("modelNo\tfixed\trandom\tDIC", sprintf("%s/DIC_%s.txt", resultsDir, modelName), row.names=F, quote=F, sep="\t")
+  files = list.files(resultsDir, pattern = ".*\\.RData", full.names = TRUE)
+  
+  for (modelFile in files)
+  {
+    i = as.numeric(gsub("(\\d*)\\.RData", "\\1", basename(modelFile)))
+    
+    load(modelFile)
+    
+    allParams = colnames(output$samples[[1]])
+    
+    areRandom = grep(".*\\[.*\\]", allParams)
+    random = unique(gsub("(.*)\\[.*\\]", "\\1", allParams[areRandom]))
+    fixed = allParams[-areRandom]
+    
+    random = paste(random, collapse=",")
+    fixed = paste(fixed, collapse=",")
+    
+    write(sprintf("%s\t%s\t%s\t%s", i, fixed, random, output$dic$deviance + output$dic$penalty),
+          file = sprintf("%s/DIC_%s.txt", resultsDir, folderName),
+          append = T)
+  }
+}
+
+signifExamine = function(modelName, top = 10, all = F, omit=NA) 
+{
+  dic = read.delim(sprintf("Results/%s/DIC_%s.txt", modelName, modelName), skip = 1)
+  dic = dic[order(dic$DIC),]
+  
+  if (!is.na(omit))
+  {
+    for (o in omit)
+    {
+      dic = dic[!(grepl(o, dic$fixed) | grepl(o, dic$random)),]
+    }
+  }
+
+  if(all)
+  {
+    dic = dic[dic$DIC <= min(dic$DIC)+2,]
+  } else {
+    dic = head(dic, top)
+  }
+  
+  dic$sig = rep(NA, nrow(dic))
+  dic$non = rep(NA, nrow(dic))
+  
+  for (i in 1:nrow(dic))
+  {
+    row = dic[i,]
+    
+    load(file=sprintf("Results/%s/%s.RData", modelName, row$modelNo))
+    
+    qts = as.data.frame(summary(output$samples)$quantiles[,c(1,5)])
+    
+    qts$sig = qts$`2.5%` * qts$`97.5%` > 0
+    qts$sign = qts$`97.5%` > 0
+    qts$param = gsub("(.*?)(?:\\[\\d*\\]|$)", "\\1", row.names(qts))
+    qts$num = gsub("(.*?)(\\[\\d*\\]|$)", "\\2", row.names(qts))
+    qts$num = gsub("\\[(\\d*)\\]", "\\1", qts$num)
+    qts$num = as.numeric(qts$num)
+    qts$num[is.na(qts$num)] = 0
+    
+    sig = c()
+    non = c()
+    nums = unique(qts$num)[!is.na(unique(qts$num))]
+    for (p in unique(qts$param))
+    {
+      sigs = qts[qts$param == p & qts$sig,]
+      if (nrow(sigs) > 0)
+      {
+        sigNums = sigs$num
+        signs = sigs$sign
+        if (all(signs == T)){
+          sign = "+"
+        }else if (all(signs == F)){
+          sign = "-"
+        }else{
+          sign = ""
+          sigNums = paste0(sigNums, sapply(signs, function(x){if (x == T) return ("+") else return("-")}))
+        }
+        
+        if (length(sigNums) == length(nums))
+        {
+          sigNums = "all"
+        }
+        if (length(sigNums == 1) & any(is.na(sigNums)))
+        {
+          sigNums = ""
+        } else {
+          sigNums = sprintf("[%s]", paste0(sigNums, collapse=","))
+        }
+        sig = c(sig, paste0(p, sigNums, sign))
+      }
+      
+      nons = qts[qts$param == p & !qts$sig,]
+      if (nrow(nons) > 0)
+      {
+        nonNums = qts[qts$param == p & !qts$sig,]$num
+        
+        if (length(nonNums) == length(nums))
+        {
+          nonNums = "all"
+        }
+        
+        if (length(nonNums == 1) & any(is.na(nonNums)))
+        {
+          nonNums = ""
+        } else {
+          nonNums = sprintf("[%s]", paste0(nonNums, collapse=","))
+        }
+        non = c(non, paste0(p, paste0(nonNums, collapse=",")))
+      }
+    }
+    
+    dic[dic$modelNo == row$modelNo,]$sig = paste0(sig, collapse=",")
+    dic[dic$modelNo == row$modelNo,]$non = paste0(non, collapse=",")
+  }
+  
+  return(dic)
+}
+
+DICexamine = function(modelName, omit=NA, all=F, top=NULL, noSig = F){
   dic = read.delim(sprintf("Results/%s/DIC_%s.txt", modelName, modelName), skip = 1)
   print(modelName)
   print(paste0("DIC minimum: ", min(dic$DIC)))
